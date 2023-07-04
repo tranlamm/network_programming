e4a798bb6885289e6bf1e39fcc24302b64f66954
tranlamm <tranlam021102eee@gmail.com>
2023-07-04 11:38:27 +0700

    done task 03.07.2023

--- 03.07.2023/ftp_client.c
+++ 03.07.2023/ftp_client.c
@@ -97,6 +97,7 @@ int main(int argc, char* argv[])
             break;
     }
     
+    // Uploading
     while (1)
     {
         printf("Enter file path to upload (type exit to escape): ");
@@ -107,7 +108,7 @@ int main(int argc, char* argv[])
         char *file_name = getFileName(buff);
         if (file_name == NULL)
         {
-            printf("Wrong file path\n");
+            printf("Invalid file path\n");
             continue;
         }
 
@@ -118,31 +119,61 @@ int main(int argc, char* argv[])
             continue;
         }
 
-        // Get port of data transfer line
-        send(clientSocket, "EPSV\r\n", 6, 0);
+        // Get port of data transfer line using EPSV
+        // send(clientSocket, "EPSV\r\n", 6, 0);
+        // ret = recv(clientSocket, buff, sizeof(buff) - 1, 0);
+        // buff[ret] = 0;
+
+        // char *pos1 = strstr(buff, "|||") + 3;
+        // char *pos2 = strchr(pos1, '|');
+        // int port_length = pos2 - pos1;
+
+        // char *port = malloc(port_length + 1);
+        // memcpy(port, pos1, port_length);
+        // port[port_length] = 0;
+
+        // Get port of data transfer line using PASV
+        send(clientSocket, "PASV\r\n", 6, 0);
         ret = recv(clientSocket, buff, sizeof(buff) - 1, 0);
         buff[ret] = 0;
 
-        char *pos1 = strstr(buff, "|||") + 3;
-        char *pos2 = strchr(pos1, '|');
-        int port_length = pos2 - pos1;
+        char *pos1 = buff;
+        int count = 0, first_byte_len = 0, second_byte_len = 0;
+        
+        while (count != 4)
+        {
+            pos1 = strchr(pos1, ',');
+            pos1 = pos1 + 1;
+            count++;
+        }
+        char *pos2 = strchr(pos1, ',');
+        first_byte_len = pos2 - pos1;
+        second_byte_len = strlen(buff) - (pos2 - buff) - 4;
 
-        char *port = malloc(port_length + 1);
-        memcpy(port, pos1, port_length);
-        port[port_length] = 0;
+        char *first_byte = malloc(first_byte_len + 1);
+        char *second_byte = malloc(second_byte_len + 1);
+        memcpy(first_byte, pos1, first_byte_len);
+        first_byte[first_byte_len] = 0;
+        memcpy(second_byte, pos2 + 1, second_byte_len);
+        second_byte[second_byte_len] = 0;
+
+        int port = 256 * atoi(first_byte) + atoi(second_byte);
+        free(first_byte);
+        free(second_byte);
 
         // Connect to data transfer line
         int dataSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
         struct sockaddr_in data_addr;
         data_addr.sin_family = AF_INET;
         data_addr.sin_addr.s_addr = inet_addr(argv[1]);
-        data_addr.sin_port = htons(atoi(port));
+        // data_addr.sin_port = htons(atoi(port));
+        data_addr.sin_port = htons(port);
 
         if (connect(dataSocket, (struct sockaddr *)&data_addr, sizeof(data_addr)) == -1)
         {
             printf("Connected failed\n");
             fclose(f);
-            free(port);
+            // free(port);
             continue;
         }
         
@@ -150,25 +181,24 @@ int main(int argc, char* argv[])
         sprintf(buff, "STOR %s\r\n", file_name);
         send(clientSocket, buff, strlen(buff), 0);
         ret = recv(clientSocket, buff, sizeof(buff) - 1, 0);
-        buff[ret] = 0;
-        printf("%s", buff);
 
         // Uploading file
         printf("Uploading ...\n");
+        unsigned long int uploaded = 0;
         while (!feof(f))
         {
             ret = fread(buff, 1, sizeof(buff), f);
             if (ret <= 0) break;
 
             send(dataSocket, buff, ret, 0);
+            uploaded += ret;
         }
         fclose(f);
         close(dataSocket);
+        printf("Uploaded %lu bytes\n", uploaded);
 
         ret = recv(clientSocket, buff, sizeof(buff) - 1, 0);
-        buff[ret] = 0;
-        printf("%s", buff);
-        free(port);
+        // free(port);
     }
 
     // QUIT
